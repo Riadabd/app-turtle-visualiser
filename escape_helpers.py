219d2bbe7189f685f54232e1fdd4ec96fc5330bd
Michaël Dierick <michael.dierick@redpencil.io>
2020-07-07 17:05:09 +0200

    Warn about cast instead of useless re-throwing exception + fix bool esc

--- escape_helpers.py
+++ escape_helpers.py
@@ -1,65 +1,57 @@
 import datetime
 import re
+from warnings import warn
 
 def sparql_escape_string(obj):
-    obj = str(obj)
+    if not isinstance(obj, str):
+        warn("You are escaping something that isn't a string with \
+        the 'sparql_escape_string'-method. Implicit casting will occurr.")
+        obj = str(obj)
     return '"""' + re.sub(r'[\\\'"]', lambda s: "\\" + s.group(0), obj) + '"""'
 
 def sparql_escape_datetime(obj):
-    if isinstance(obj, datetime.datetime):
-        obj = obj.replace(microsecond=0) # xsd doesn't support microseconds in ISO time format
-        return '"{}"^^xsd:dateTime'.format(obj.isoformat())
-    try:
+    if not isinstance(obj, datetime.datetime):
+        warn("You are escaping something that isn't a datetime with \
+        the 'sparql_escape_datetime'-method. Implicit casting will occurr.")
         obj = datetime.datetime.strptime(str(obj), "%Y-%m-%dT%H:%M:%S")
-        return '"{}"^^xsd:dateTime'.format(obj.isoformat())
-    except ValueError as e: # Failed casting to string or invalid dateTime format
-        raise e
+    obj = obj.replace(microsecond=0) # xsd doesn't support microseconds in ISO time format
+    return '"{}"^^xsd:dateTime'.format(obj.isoformat())
 
 def sparql_escape_date(obj):
-    if isinstance(obj, datetime.date):
-        return '"{}"^^xsd:date'.format(obj.isoformat())
-    try:
+    if not isinstance(obj, datetime.date):
+        warn("You are escaping something that isn't a date with \
+        the 'sparql_escape_date'-method. Implicit casting will occurr.")
         obj = datetime.datetime.strptime(str(obj), "%Y-%m-%d").date()
-        return '"{}"^^xsd:date'.format(obj.isoformat())
-    except ValueError as e: # Failed casting to string or invalid date format
-        raise e
+    return '"{}"^^xsd:date'.format(obj.isoformat())
 
 def sparql_escape_time(obj):
-    if isinstance(obj, datetime.time):
-        obj = obj.replace(microsecond=0) # xsd doesn't support microseconds in ISO time format
-        return '"{}"^^xsd:time'.format(obj.isoformat())
-    try:
+    if not isinstance(obj, datetime.time):
+        warn("You are escaping something that isn't a time with \
+        the 'sparql_escape_time'-method. Implicit casting will occurr.")
         obj = datetime.datetime.strptime(str(obj), "%H:%M:%S").time()
-        return '"{}"^^xsd:time'.format(obj.isoformat())
-    except ValueError as e: # Failed casting to string or invalid time format
-        raise e
+    obj = obj.replace(microsecond=0) # xsd doesn't support microseconds in ISO time format
+    return '"{}"^^xsd:time'.format(obj.isoformat())
 
 def sparql_escape_int(obj):
-    if isinstance(obj, int):
-        return '"{}"^^xsd:integer'.format(obj)
-    try:
+    if not isinstance(obj, int):
+        warn("You are escaping something that isn't an int with \
+        the 'sparql_escape_int'-method. Implicit casting will occurr.")
         obj = str(int(obj))
-        return '"{}"^^xsd:integer'.format(obj)
-    except ValueError as e: # Failed casting
-        raise e
+    return '"{}"^^xsd:integer'.format(obj)
 
 def sparql_escape_float(obj):
-    if isinstance(obj, float):
-        return '"{}"^^xsd:float'.format(obj)
-    try:
+    if not isinstance(obj, int):
+        warn("You are escaping something that isn't a float with \
+        the 'sparql_escape_float'-method. Implicit casting will occurr.")
         obj = str(float(obj))
-        return '"{}"^^xsd:float'.format(obj)
-    except ValueError as e: # Failed casting
-        raise e
+    return '"{}"^^xsd:float'.format(obj)
 
 def sparql_escape_bool(obj):
-    if isinstance(obj, bool):
-        return '"{}"^^xsd:boolean'.format(obj)
-    try:
-        obj = str(bool(obj))
-        return '"{}"^^xsd:boolean'.format(obj)
-    except ValueError as e: # Failed casting
-        raise e
+    if not isinstance(obj, bool):
+        warn("You are escaping something that isn't a bool with \
+        the 'sparql_escape_bool'-method. Implicit casting will occurr.")
+        obj = bool(obj)
+    return '"{}"^^xsd:boolean'.format("true" if obj else "false")
 
 def sparql_escape_uri(obj):
     obj = str(obj)
